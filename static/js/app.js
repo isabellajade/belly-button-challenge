b21a25f052b4af533e4fb8d1f352b6118b8c0c92
isabellajade <isabellajadet@gmail.com>
2023-02-05 13:13:42 -0600

    data

---
+++ static/js/app.js
@@ -0,0 +1,3 @@
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((resp) => {
+    console.log(resp)
+})
