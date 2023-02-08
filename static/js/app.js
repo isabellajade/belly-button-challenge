71dd55f0a39681fca6a6cfde223f7ee84e04b8be
isabellajade <isabellajadet@gmail.com>
2023-02-07 22:19:46 -0600

    bubble chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,86 @@
+var data;
 d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((resp) => {
     console.log(resp)
+    data = resp
+    let dropDown = d3.select("#selDataset")
+    resp.names.forEach((name) => {
+        dropDown.append("option").text(name)
+    })
+    buildChart(resp.names[0])
 })
+
+function optionChanged(selectedID) {
+    buildChart(selectedID)
+}
+
+let buildChart = (selectedID) => {
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((resp) => {
+        console.log(resp)
+        data = resp
+        let dropDown = d3.select("#selDataset")
+        resp.names.forEach((name) => {
+            dropDown.append("option").text(name)
+        })
+        if (!data) {
+            return
+        }
+        var filteredData = data.samples.filter(row => row.id == selectedID)[0]
+        let x = filteredData.sample_values.slice(0, 10).reverse()
+        let y = filteredData.otu_ids.slice(0, 10).map(id => "OTU " + id).reverse()
+        let hoverText = filteredData.otu_labels.slice(0, 10).reverse()
+
+        let barData = [
+            {
+                x: x,
+                y: y,
+                text: hoverText,
+                type: "bar",
+                orientation: "h"
+            }
+        ]
+        let layout = {
+            title: "title"
+        }
+        let config = {
+            responsive: true
+        }
+
+        Plotly.newPlot("bar", barData, layout, config)
+
+            // bubble chart
+            
+        let trace1 = {
+            x: filteredData.otu_ids,
+            y: filteredData.sample_values,
+            text: filteredData.otu_labels,
+            mode: 'markers',
+            marker: {
+                color: filteredData.otu_ids,
+                size: filteredData.sample_values,
+                colorscale: "Earth"
+            }
+        };
+
+        var data = [trace1];
+
+        var layout1 = {
+            title: 'Marker Size',
+            showlegend: false
+        };
+
+        let config1 = {
+            responsive: true
+        }
+
+        Plotly.newPlot('bubble', data, layout1, config1)
+    });
+
+}
+
+
+let buildBubble = (selectID) => {
+    var filteredData1 = data.samples.filter(row => row.id == selectID)[0]
+    let y1 = filteredData1.sample_values
+    let x1 = filteredData1.otu_ids
+    let hoverText1 = filteredData1.otu_labels
+}
